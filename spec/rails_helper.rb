91d121289556d73160460411da8be62c2b0b1209
Andrew Woods <andrewwoods88@gmail.com>
2020-06-14 13:34:10 -0400

    setting up tests and controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,12 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configuration do |config|
+  config.include RequestSpecHelper, type: :request
+end
+
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
